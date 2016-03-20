c7aa3ce5253cca3f87593363ea7a181f5b97f1e0
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-03-19 23:30:17 -0400

    I always forget we've moved on...

---
+++ src/pebble-ttc.c
@@ -0,0 +1,133 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static SimpleMenuLayer *s_simple_menu_layer;
+
+#define NUM_MENU_SECTIONS 2
+#define NUM_FIRST_MENU_ITEMS 2
+#define NUM_SECOND_MENU_ITEMS 4
+
+static SimpleMenuSection s_menu_sections[NUM_MENU_SECTIONS];
+static SimpleMenuItem s_first_menu_items[NUM_FIRST_MENU_ITEMS];
+static SimpleMenuItem s_second_menu_items[NUM_SECOND_MENU_ITEMS];
+// static GBitmap *s_menu_icon_image;
+
+static bool s_special_flag = false;
+static int s_hit_count = 0;
+
+static void menu_select_callback(int index, void *ctx) {
+  s_first_menu_items[index].subtitle = "You've hit select here!";
+  layer_mark_dirty(simple_menu_layer_get_layer(s_simple_menu_layer));
+}
+
+static void special_select_callback(int index, void *ctx) {
+  // Of course, you can do more complicated things in a menu item select callback
+  // Here, we have a simple toggle
+  s_special_flag = !s_special_flag;
+
+  SimpleMenuItem *menu_item = &s_second_menu_items[index];
+
+  if (s_special_flag) {
+    menu_item->subtitle = "Okay, it's not so special.";
+  } else {
+    menu_item->subtitle = "Well, maybe a little.";
+  }
+
+  if (++s_hit_count > 5) {
+    menu_item->title = "Very Special Item";
+  }
+
+  layer_mark_dirty(simple_menu_layer_get_layer(s_simple_menu_layer));
+}
+
+
+static void main_window_load(Window *window) {
+  // s_menu_icon_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_1);
+
+  // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
+  // an int as such to easily change the order of menu items later
+  int num_a_items = 0;
+
+  s_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+    .title = "509-Harbourfront",
+    .subtitle = "West - 509/511 Replacement Bus towards Lake Shore and Bathurst",
+    .callback = menu_select_callback,
+  };
+  s_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+    .title = "511-Bathrust",
+    .subtitle = "North - 511 Bathurst towards Bathurst Station",
+    // ,
+    // .subtitle = "Here's a subtitle",
+    .callback = menu_select_callback,
+  };
+  // s_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+  //   .title = "Third Item",
+  //   .subtitle = "This has an icon",
+  //   .callback = menu_select_callback //,
+  //   // .icon = s_menu_icon_image,
+  // };
+
+  s_second_menu_items[0] = (SimpleMenuItem) {
+    .title = "Eastbound on King at Spadina",
+    .callback = special_select_callback,
+  };
+
+  s_second_menu_items[1] = (SimpleMenuItem) {
+    .title = "Northbound on Spadina at King Farside",
+    .callback = special_select_callback,
+  };
+
+  s_second_menu_items[2] = (SimpleMenuItem) {
+    .title = "Southbound on Spadina at King",
+    .callback = special_select_callback,
+  };
+
+  s_second_menu_items[3] = (SimpleMenuItem) {
+    .title = "Westbound on King at Spadina",
+    .callback = special_select_callback,
+  };
+
+
+  s_menu_sections[0] = (SimpleMenuSection) {
+    .title = "Fleet St At Bastion St",
+    .num_items = NUM_FIRST_MENU_ITEMS,
+    .items = s_first_menu_items,
+  };
+  s_menu_sections[1] = (SimpleMenuSection) {
+    .title = "King and Spadina",
+    .num_items = NUM_SECOND_MENU_ITEMS,
+    .items = s_second_menu_items,
+  };
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  s_simple_menu_layer = simple_menu_layer_create(bounds, window, s_menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  layer_add_child(window_layer, simple_menu_layer_get_layer(s_simple_menu_layer));
+}
+
+static void main_window_unload(Window *window) {
+  simple_menu_layer_destroy(s_simple_menu_layer);
+}
+
+void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+
+  return 0;
+}
