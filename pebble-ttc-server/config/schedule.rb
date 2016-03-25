a6ad58a956d8ee5b6f698d801c8e356261578509
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-03-25 02:38:44 -0400

    Less aggressive schedule; accessible log dir

--- pebble-ttc-server/config/schedule.rb
+++ pebble-ttc-server/config/schedule.rb
@@ -1,5 +1,5 @@
-set :output, '/var/log/pebble-ttc-cronjobs.log'
+set :output, '~/pebble-ttc-cronjobs.log'
 
-every 1.day, :at => '2:15 am' do
+every :monday, :at => '2:15 am' do
   rake "db:reset ttc:crawl"
 end
