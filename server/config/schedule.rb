e2358e0a023bcd2ade179bc5b4b14ab1d3f6ba8f
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-03-26 11:01:12 -0400

    Restart server after crawling

--- server/config/schedule.rb
+++ server/config/schedule.rb
@@ -1,4 +1,11 @@
-job_type :rake_without_db_env_check, "cd :path && DISABLE_DATABASE_ENVIRONMENT_CHECK=1 :environment_variable=:environment bundle exec rake :task --silent :output"
+RAKE_CRAWLER_ENV =
+  "DISABLE_DATABASE_ENVIRONMENT_CHECK=1 " \
+  ":environment_variable=:environment"
+
+job_type :rake_without_db_env_check,
+  "cd :path && " \
+  "#{RAKE_CRAWLER_ENV} bundle exec rake :task --silent :output && " \
+  "sudo supervisorctl restart pebblettc"
 
 set :output, '~/pebble-ttc-cronjobs.log'
 
