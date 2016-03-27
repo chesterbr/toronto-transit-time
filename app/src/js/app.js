e6d88234a2831c1ffc13b68bde25cdd64e6f9039
Danilo Cabello <danilo.cabello@gmail.com>
2016-03-26 22:20:00 -0400

    Rename getLocation to getCoordinates, only pass latitude and longitude.

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -3,15 +3,11 @@ geo = require('geo');
 bluetooth = require('bluetooth');
 
 function openRoutesList(e) {
-  geo.getLocation(function(loc) {
-    internet.getRoutes(loc.coords.latitude, loc.coords.longitude, function(routes) {
+  geo.getCoordinates(function(latitude, longitude) {
+    internet.getRoutes(latitude, longitude, function(routes) {
       bluetooth.sendRoutes(routes);
     })
   });
 }
 
 Pebble.addEventListener('ready', openRoutesList);
-
-
-
-
--- app/src/js/geo.js
+++ app/src/js/geo.js
@@ -1,16 +1,18 @@
-function getLocation(callback) {
 var timeout = 15000;
 var maximumAge = 60000;
+
+function getCoordinates(callback) {
   navigator.geolocation.getCurrentPosition(
-    callback,
+    function(position) {
+      callback(position.coords.latitude, position.coords.longitude)
+    },
     locationError,
-    {timeout: 15000, maximumAge: 60000}
     {timeout: timeout, maximumAge: maximumAge}
   );
-}
+};
 
 function locationError(err) {
   console.log('Error requesting location!');
-}
+};
 
-module.exports.getLocation = getLocation;
+module.exports.getLocation = getCoordinates;
