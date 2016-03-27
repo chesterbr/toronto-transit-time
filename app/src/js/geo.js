6f9742ff67335a57cde658ec99754ae8976e64e2
Danilo Cabello <danilo.cabello@gmail.com>
2016-03-26 22:19:23 -0400

    Extract configuration variables for timeout and maximumAge.

--- app/src/js/geo.js
+++ app/src/js/geo.js
@@ -1,8 +1,11 @@
 function getLocation(callback) {
+var timeout = 15000;
+var maximumAge = 60000;
   navigator.geolocation.getCurrentPosition(
     callback,
     locationError,
     {timeout: 15000, maximumAge: 60000}
+    {timeout: timeout, maximumAge: maximumAge}
   );
 }
 
