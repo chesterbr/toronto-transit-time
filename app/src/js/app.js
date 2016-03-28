8b57af18078a958d07b488b5d2fac87f8b17ee7a
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-03-28 00:52:39 -0400

    Improved js module naming (and a skeleton for the predictions wiring)

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,14 +1,20 @@
-internet = require('internet');
-geo = require('geo');
-bluetooth = require('bluetooth');
+internet = require('internet'); // Communication with back-end
+watch    = require('watch');    // Communication with watch
+geo      = require('geo');      // Geolocation
 
 function openRoutesList(e) {
   geo.getLocation(function(loc) {
-    internet.getRoutes(loc.coords.latitude, loc.coords.longitude, function(routes) {
-      bluetooth.sendRoutes(routes);
-    })
+    internet.getRoutes(loc.coords.latitude, loc.coords.longitude,
+      watch.sendRoutes
+    );
   });
 }
+
+// watch.routeSelected = function(route) {
+//   internet.getPredictions(route,
+//     watch.sendPredictions
+//   );
+// }
 
 Pebble.addEventListener('ready', openRoutesList);
 
