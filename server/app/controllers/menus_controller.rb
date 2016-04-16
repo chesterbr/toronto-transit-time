c34def010ab3e828da3ea3497a5d86aa7cab7935
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-04-16 01:03:24 -0400

    Thin controller by moving code to model (and splitting in scopes and smaller methods)

--- server/app/controllers/menus_controller.rb
+++ server/app/controllers/menus_controller.rb
@@ -1,65 +1,8 @@
 class MenusController < ApplicationController
   def show
     lat, lon = params[:lat].to_f, params[:lon].to_f
-    lat_range, lon_range = square(lat, lon, 500)
-
-    stops_and_routes =
-      Stop
-        .where(lat: lat_range, lon: lon_range)
-        .joins(directions: :route)
-        .order(distance_criteria(lat,lon))
-        .select('stops.title      stop,     ' \
-                'routes.title     route,    ' \
-                'directions.title direction,' \
-                'routes.tag       r,        ' \
-                'stops.tag        s         ')
-        .group_by(&:stop)
-        .to_a.map do |stop|
-          {
-            stop: stop[0],
-            routes: stop[1].map { |route| route
-                .slice(*%w(route direction))
-                .merge({uri: predictions_uri(route)})
-            }
-          }
-        end
+    stops_and_routes = Stop.as_pebble_menu(lat,lon)
 
     render json: stops_and_routes.to_json
-  end
-
-  private
-
-  def distance_criteria(lat, lon)
-    d1 = "(lat-(#{lat.to_f}))"
-    d2 = "(lon-(#{lon.to_f}))"
-
-    "(#{d1}*#{d1})+(#{d2}*#{d2})"
-  end
-
-  def predictions_uri(route)
-    params = route.as_json.slice('r', 's').merge({
-      command: :predictions,
-      a: :ttc
-    })
-    "http://webservices.nextbus.com/service/publicXMLFeed?#{params.to_query}"
-  end
-
-  def square(lat, lon, distance_meters)
-    distance = meters_to_earth_degrees(distance_meters)
-    delta_lon = distance / (Math.cos(degrees_to_radians(lat))).abs
-    delta_lat = distance
-
-    [
-      (lat - delta_lat..lat + delta_lat),
-      (lon - delta_lon..lon + delta_lon)
-    ]
-  end
-
-  def meters_to_earth_degrees(distance)
-    distance * 0.00000900537681
-  end
-
-  def degrees_to_radians(angle)
-    angle * Math::PI / 180
   end
 end
--- server/app/models/stop.rb
+++ server/app/models/stop.rb
@@ -1,3 +1,53 @@
 class Stop < ApplicationRecord
   has_and_belongs_to_many :directions
+
+  scope :within_fence_ordered_by_distance, ->(lat, lon, distance) {
+    lat_range, lon_range = Geo.square(lat, lon, 500)
+    order_expr = Geo.distance_criteria(lat,lon)
+
+    where(lat: lat_range, lon: lon_range)
+      .order(order_expr)
+  }
+
+  scope :with_routes_and_directions_grouped_by_address, -> {
+    joins(directions: :route)
+      .select('stops.title      stop,     ' \
+              'routes.title     route,    ' \
+              'directions.title direction,' \
+              'routes.tag       r,        ' \
+              'stops.tag        s         ')
+      .group_by(&:stop)
+  }
+
+  class << self
+    def as_pebble_menu(lat, lon)
+      stops_array = Stop.within_fence_ordered_by_distance(lat, lon, 500)
+                        .with_routes_and_directions_grouped_by_address
+                        .to_a
+
+      hashes_with_stop_and_routes(stops_array)
+    end
+
+    private
+
+    def hashes_with_stop_and_routes(stops)
+      stops.map do |stop|
+        {
+          stop: stop[0],
+          routes: stop[1].map { |route| route
+              .slice(*%w(route direction))
+              .merge({uri: predictions_uri(route)})
+          }
+        }
+      end
+    end
+
+    def predictions_uri(route)
+      params = route.as_json.slice('r', 's').merge({
+        command: :predictions,
+        a: :ttc
+      })
+      "http://webservices.nextbus.com/service/publicXMLFeed?#{params.to_query}"
+    end
+  end
 end
