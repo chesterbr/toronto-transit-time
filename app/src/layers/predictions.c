970c1f8d2e73c322769b9c8bf04a53525a58ddd3
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2016-04-25 21:10:37 -0400

    Update (some of) the text fields

--- app/src/layers/predictions.c
+++ app/src/layers/predictions.c
@@ -1,10 +1,11 @@
 #include "predictions.h"
 
+const int PREDICTION_TEXT_SIZE = 15;
+
 static TextLayer *s_main_text_layer;
 static TextLayer *s_secondary_text_layer;
 static TextLayer *s_first_prediction_text_layer;
 static TextLayer *s_other_predictions_text_layer;
-static ScrollLayer *s_scroll_layer;
 static ContentIndicator *s_indicator;
 static Layer *s_indicator_up_layer;
 static Layer *s_indicator_down_layer;
@@ -14,31 +15,19 @@ static GRect s_predictions_layer_bounds;
 static GRect s_first_prediction_text_bounds;
 static GRect s_other_predictions_text_bounds;
 
-static char s_predictions_full_text[4000];
-static char* s_buffer_pos;
-static int s_buffer_size;
+static DisplayableItem *s_items;
+static int s_items_count;
+static int s_current_item;
+static char *s_first_prediction_text;
+static char *s_other_predictions_text;
 
-static DisplayableItem *items[];
-static int items_count;
-
-static void update_contents(DisplayableItem items[], int count);
-static void update_scroll_bounds(void);
-static void text_reset(void);
-static void text_append_number(int number);
-static void text_append_str(char* text);
-static void text_append(char* text, int number);
+static void update_current_display_item();
+static int format_time(char* var, DisplayableItem item, int index);
 static void fill_background(Layer *layer, GContext *ctx);
-
-static char s_content[] = "Starting at 2am on March 29 until 5am on April 11 - 504 King streetcars will be on diversion due to work at King St W at Charlotte St. EB: Spadina, Queen and Church. WB: York, Queen and Spadina. Visit www.ttc.ca for details.";
 
 void predictions_layer_init(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   s_bounds = layer_get_bounds(window_layer);
-
-  // s_scroll_layer = scroll_layer_create(s_bounds);
-  // scroll_layer_set_click_config_onto_window(s_scroll_layer, window);
-  // scroll_layer_set_shadow_hidden(s_scroll_layer, true);
-  // layer_add_child(window_layer, scroll_layer_get_layer(s_scroll_layer));
 
   GRect main_text_bounds = GRect(
     s_bounds.origin.x, STATUS_BAR_LAYER_HEIGHT,
@@ -119,18 +108,9 @@ void predictions_layer_init(Window *window) {
   text_layer_enable_screen_text_flow_and_paging(s_main_text_layer, 2);
   text_layer_enable_screen_text_flow_and_paging(s_secondary_text_layer, 2);
 
-  // This is just a test
-
-
-  content_indicator_set_content_available(s_indicator, ContentIndicatorDirectionUp,
-                                          true);
-  content_indicator_set_content_available(s_indicator, ContentIndicatorDirectionDown,
-                                          true);
-
-  text_layer_set_text(s_main_text_layer, "East - 306 Carlton Blue Night towards Main Street Station");
-  text_layer_set_text(s_secondary_text_layer, "College St At Beverley St And Some Extra Stuff");
-  text_layer_set_text(s_first_prediction_text_layer, "1min");
-  text_layer_set_text(s_other_predictions_text_layer, "69min\n88min");
+  // Non-UI init
+  s_first_prediction_text = malloc(sizeof(char) * PREDICTION_TEXT_SIZE);
+  s_other_predictions_text = malloc(sizeof(char) * PREDICTION_TEXT_SIZE);
 }
 
 static void fill_background(Layer *layer, GContext *ctx) {
@@ -157,70 +137,44 @@ static void fill_background(Layer *layer, GContext *ctx) {
 }
 
 void predictions_layer_update(DisplayableItem *items, int count, bool reset_scroll) {
-  update_contents(items, count);
+  s_items = items;
+  s_items_count = count;
+
   if (reset_scroll) {
-    update_scroll_bounds();
-    scroll_layer_set_content_offset(s_scroll_layer, GPointZero, false);
+    s_current_item = 0;
   }
+
+  update_current_display_item();
 }
 
 // Private
 
-static void update_contents(DisplayableItem *items, int count) {
-  text_reset();
-  for(int i = 0; i < count; i++) {
-    if (i > 0) {
-      text_append_str("\n_________\n\n");
-    }
-    text_append_str(items[i].text);
-    text_append_str("\n");
-    if (items[i].times_count == 0 && items[i].is_prediction) {
-      text_append_str("\nNo predictions");
-    }
-    for (int j = 0; j < items[i].times_count; j++) {
-      text_append_str(j == 0 ? "\n" : " / ");
-      if (items[i].times[j] >= 60) {
-        text_append_number(items[i].times[j] / 60);
-        text_append_str("min");
-      } else if (items[i].times[j] >= 0) {
-        text_append_number(items[i].times[j]);
-        text_append_str("s");
-      } else {
-        text_append_str("DUE");
-      }
-      items[i].times[j]--;
-    }
-  }
-  text_layer_set_text(s_main_text_layer, s_predictions_full_text);
-}
-
-static void update_scroll_bounds(void) {
-  int content_size = 5000;
-  text_layer_set_size(s_main_text_layer, GSize(s_bounds.size.w, content_size));
-  scroll_layer_set_content_size(s_scroll_layer, GSize(s_bounds.size.w, content_size));
-}
-
-static void text_reset(void) {
-  s_buffer_size = 4000;
-  s_buffer_pos = s_predictions_full_text;
-}
-
-static void text_append_number(int number) {
-  text_append(NULL, number);
-}
-
-static void text_append_str(char* text) {
-  text_append(text, 0);
-}
-
-static void text_append(char* text, int number) {
-  int appended;
-  if (text != NULL) {
-    appended = snprintf(s_buffer_pos, s_buffer_size, "%s", text);
+static void update_current_display_item() {
+  DisplayableItem item = s_items[s_current_item];
+  text_layer_set_text(s_main_text_layer, item.text);
+  if (item.is_prediction) {
+    format_time(s_first_prediction_text, item, 0);
+    format_time(s_other_predictions_text, item, 1);
+    text_layer_set_text(s_secondary_text_layer, "TODO put stop address");
+    text_layer_set_text(s_first_prediction_text_layer, s_first_prediction_text);
+    text_layer_set_text(s_other_predictions_text_layer, s_other_predictions_text);
   } else {
-    appended = snprintf(s_buffer_pos, s_buffer_size, "%d", number);
+    text_layer_set_text(s_secondary_text_layer, NULL);
+    text_layer_set_text(s_first_prediction_text_layer, NULL);
+    text_layer_set_text(s_other_predictions_text_layer, NULL);
   }
-  s_buffer_size -= appended;
-  s_buffer_pos += appended;
 }
 
+static int format_time(char* var, DisplayableItem item, int index) {
+  char* format;
+  int value = item.times[index];
+  if (value >= 60) {
+    format = "%dmin";
+    value = value / 60;
+  } else if (value > 0) {
+    format = "%ds";
+  } else {
+    format = "DUE";
+  }
+  return snprintf(var, PREDICTION_TEXT_SIZE, format, value);
+}
