ec20c81389c030a4b3775403e035ad9ad29b80e4
Chester (Carlos Duarte do Nascimento) <cd@pobox.com>
2019-03-06 10:42:48 -0500

    Apparently Rails likes a nimbler schema.rb format now ¯\_(ツ)_/¯

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,33 +16,29 @@ ActiveRecord::Schema.define(version: 20160319162500) do
     t.string  "tag"
     t.string  "title"
     t.integer "route_id"
+    t.index ["route_id"], name: "index_directions_on_route_id"
+    t.index ["tag"], name: "index_directions_on_tag"
   end
-
-  add_index "directions", ["route_id"], name: "index_directions_on_route_id"
-  add_index "directions", ["tag"], name: "index_directions_on_tag"
 
   create_table "directions_stops", id: false, force: :cascade do |t|
     t.integer "direction_id"
     t.integer "stop_id"
+    t.index ["direction_id"], name: "index_directions_stops_on_direction_id"
+    t.index ["stop_id"], name: "index_directions_stops_on_stop_id"
   end
-
-  add_index "directions_stops", ["direction_id"], name: "index_directions_stops_on_direction_id"
-  add_index "directions_stops", ["stop_id"], name: "index_directions_stops_on_stop_id"
 
   create_table "routes", force: :cascade do |t|
     t.string "tag"
     t.string "title"
+    t.index ["tag"], name: "index_routes_on_tag"
   end
-
-  add_index "routes", ["tag"], name: "index_routes_on_tag"
 
   create_table "stops", force: :cascade do |t|
     t.string "tag"
     t.string "title"
     t.float  "lat"
     t.float  "lon"
+    t.index ["tag"], name: "index_stops_on_tag"
   end
-
-  add_index "stops", ["tag"], name: "index_stops_on_tag"
 
 end
