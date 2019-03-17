4b1a9de49e88cbfa617fc5aff35b17390913ccb3
Chester (Carlos Duarte do Nascimento) <cd@pobox.com>
2019-03-17 11:48:29 -0400

    Start app via Docker Compose

--- server/config/boot.rb
+++ server/config/boot.rb
@@ -3,3 +3,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 # Disabled this because this app is tiny
 # require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Play nicely with Docker composer by not buffering output
+$stdout.sync = true
